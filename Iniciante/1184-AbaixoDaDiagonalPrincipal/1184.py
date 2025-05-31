3ab23fba39850ba643726a273891666c9a6e8e61
Paulo Anjos <pauloanjos.ufopa@gmail.com>
2025-05-30 21:57:00 -0300

    Create 1184.py

---
+++ Iniciante/1184-AbaixoDaDiagonalPrincipal/1184.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+O = input()
+MATRIZ = [[0.0 for _ in range(12)]for _ in range(12)]
+
+for linha in range(12):
+    for coluna in range(12):
+        MATRIZ[linha][coluna] = float(input())
+
+soma = 0.0
+contador = 0
+
+for linha in range(12):
+    for coluna in range(12):
+        if linha > coluna:
+            soma += MATRIZ[linha][coluna]
+            contador += 1
+
+if O == "S":
+    print(f"{soma:.1f}")
+else:
+    print(f"{soma/contador:.1f}")
